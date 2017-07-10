bc6f2387c204153b7cd683c8e4fd58e0d288f267
Paulo A Pereira <odesassossegado@gmail.com>
2017-07-10 20:45:03 +0100

    Add base service files

---
+++ demo_flask_app/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='Demo-App',
+    description='Demmo App',
+    version='0.1',
+    packages=['demo'],
+    install_requires=[
+        'Flask'
+    ]
+)
